45ba67aa58c9e33be6a45808db0a46b64b6d2121
Jonathan Bartolo <jonathan.bartolo@stmartin.edu>
2024-11-13 09:11:05 -0800

    First Commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,54 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Sorting Project");
+            int[] arr = { 2, 1, 5, 4 };
+            Program.Print(arr);
+            int[] a = { 1, 3, 5, 7 };
+            int[] b = { 2, 4, 6, 8 };
+            int[] c = new int[a.Length + b.Length];
+        }
+
+        static void Merge(int[] a, int[] b, int[] c)
+        {
+            int i = 0; int j = 0; int k = 0;
+            while (i < a.Length && j < b.Length)
+            {
+                if (a[i] < b[j])
+                {
+                    c[k] = a[i];
+                    i++;
+                    k++;
+                }
+                else
+                {
+                    c[k] = b[j];
+                    j++;
+                    k++;
+                }
+            }
+
+            while (i < a.Length)
+            {
+                c[k] = a[i];
+                i++;
+                k++;
+            }
+
+            while (j < b.Length)
+            {
+                c[k] = b[j];
+                j++;
+                k++;
+            }
+        }
+
+        static void Print(int[] arr)
+        {
+            foreach (int c in arr)
+            {
+                Console.Write(c);
+            }
         }
     }
 }
